58eca564eb940c5e1e8333d143f4311525707225
xs1523c <xs1523c@gmail.com>
2018-03-19 21:27:29 +0800

    add restaurants resources for admin

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,13 @@
 Rails.application.routes.draw do
   devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  resources :restaurants
+ 
   root "restaurants#index"
 
 
 
   namespace :admin do
+    resources :restaurants
     root "restaurants#index"
   end
 
