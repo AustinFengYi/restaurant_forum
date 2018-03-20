1105830562f845fa229caf75b1902d73b0da1007
xs1523c <xs1523c@gmail.com>
2018-03-20 14:02:39 +0800

    create category midel

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180319122455) do
+ActiveRecord::Schema.define(version: 20180320055829) do
+
+  create_table "categories", force: :cascade do |t|
+    t.string "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "restaurants", force: :cascade do |t|
     t.string "name"
@@ -20,6 +26,7 @@ ActiveRecord::Schema.define(version: 20180319122455) do
     t.text "description"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "category_id"
   end
 
   create_table "users", force: :cascade do |t|
