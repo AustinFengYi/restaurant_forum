8acf10302061cefca5c0e779b9e14cbb6fdd5c10
xs1523c <xs1523c@gmail.com>
2018-04-05 23:42:22 +0800

    add partial for follow / unfollow button

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,6 +7,10 @@ class User < ApplicationRecord
   def admin?
     self.role == "admin"
   end
+
+  def following?(user)
+    self.followings.include?(user)
+  end
   
  # 如果 User 已經有了評論，就不允許刪除帳號（刪除時拋出 Error）
   has_many :comments, dependent: :restrict_with_error
